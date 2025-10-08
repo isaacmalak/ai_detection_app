1d6c172d45cf49fddbbc04f2642d26e12e2a598a
Isaac Malak <isaacmalak123@gmail.com>
2025-10-08 16:41:45 +0300

    feat: initial styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:ai_detection_app/theme/font_wrapper.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +12,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.light,
-        ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        useMaterial3: true,
+        fontFamily: "VT323",
+        textTheme: GoogleFonts.vt323TextTheme(),
       ),
-      darkTheme: ThemeData(brightness: Brightness.dark, useMaterial3: true),
+      themeMode: ThemeMode.dark,
       showSemanticsDebugger: false,
       home: const MyHomePage(title: 'AI detection prototype'),
     );
@@ -39,12 +41,15 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
 
-        title: Text(widget.title, style: TextStyle(fontFamily: 'Roboto')),
+        title: Text(
+          widget.title,
+          style: fontWrapper(context, const TextStyle(fontSize: 24)),
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [const Text('You have pushed the button this many times:')],
+          children: [Text("this is a test ")],
         ),
       ),
     );
---
+++ lib/theme/font_wrapper.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+/// A wrapper function is used for changing the font style easily in the future
+/// for example if we want to change the font from VT323 to another font
+/// we just need to change it here not in the whole app
+///
+/// Usage:
+/// ```dart
+///   Text(
+///      "Hello World",
+///       style: fontWrapper(context, TextStyle(fontSize: 24))
+///   );
+/// ```
+
+fontWrapper(BuildContext context, TextStyle style) {
+  return GoogleFonts.vt323().merge(style);
+}
